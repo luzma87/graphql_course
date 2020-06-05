6122262b2ba51a73fe022775a19886c06130cb76
luz <luzma_87@yahoo.com>
2020-06-05 16:36:35 -0500

    update

--- graphql-basics/src/resolvers/Mutation.js
+++ graphql-basics/src/resolvers/Mutation.js
@@ -13,6 +13,24 @@ const Mutation = {
     users.push(user);
     return user;
   },
+  updateUser(parent, args, ctx, info) {
+    const { id, data } = args;
+    const { users } = ctx.db;
+    const user = users.find((user) => user.id === id);
+    if (!user) throw new Error('User not found');
+    if (typeof data.email === 'string') {
+      const emailTaken = users.some((user) => user.email === data.email);
+      if (emailTaken) throw new Error('Email is taken');
+      user.email = data.email;
+    }
+    if (typeof data.name === 'string') {
+      user.name = data.name;
+    }
+    if (typeof data.age !== 'undefined') {
+      user.age = data.age;
+    }
+    return user;
+  },
   deleteUser(parent, args, ctx, info) {
     let { users, posts, comments } = ctx.db;
     const { id } = args;
